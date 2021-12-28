543423eb8ab1183cdd7dcecd0fc95b722aa772ef
Bobby T <bob.tarino@gmail.com>
2021-12-28 17:08:09 -0600

    created setup.js and defined global variables

---
+++ setup.js
@@ -0,0 +1,51 @@
+// target first canvas
+const canvas = document.getElementById('canvas1');
+const ctx1 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+// target second canvas
+const canvas = document.getElementById('canvas2');
+const ctx2 = canvas.getContext('2d');
+canvas2.width = 600;
+canvas2.height = 600;
+
+// target third canvas
+const canvas = document.getElementById('canvas3');
+const ctx3 = canvas.getContext('2d');
+canvas3.width = 600;
+canvas3.height = 600;
+
+// target fourth canvas
+const canvas = document.getElementById('canvas4');
+const ctx4 = canvas.getContext('2d');
+canvas4.width = 600;
+canvas4.height = 600;
+
+// target fifth canvas
+const canvas = document.getElementById('canvas5');
+const ctx5 = canvas.getContext('2d');
+canvas5.width = 600;
+canvas5.height = 600;
+
+// global variables
+
+// 80x80 pixels
+const grid = 80;
+let keys = [];
+let score = 0;
+let collisionsCount = 0;
+let frame = 0;
+// game speed will increase slightly when point is scored
+let gameSpeed = 1;
+
+// particles array will hold all particle objects for dust fx
+const particlesArray = [];
+// limits how many to make to prevent performance issues
+const maxParticles = 300;
+// ripples array will hold all water ripple particle objects for river fx
+const ripplesArray =  [];
+// cars array holds all car objects 
+const carsArray = [];
+// logs array holds log and turtle objects 
+const logsArray = [];
